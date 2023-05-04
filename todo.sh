09fce38851ed312a3c0261441d2281de0967498e
shiva <shiva.r.160899@gmail.com>
2023-05-05 00:38:37 +0530

    fixed CRUD task functions and made to work properly

--- todo.sh
+++ todo.sh
@@ -37,37 +37,48 @@ function showTask(){
 }
 
 function emptyTask(){
-    truncate -s0 $file $completed
+    printf "Clear content for todo(t)/completed(c)/others will clear both?\n"
+    read -p "t/c》" operation
+    if [[ $operation == 'c' ]]; then
+        truncate -s0 $completed
+    elif [[ $operation == 't' ]]; then
+        truncate -s0 $file
+    else
+        truncate -s0 $file $completed
+    fi
 }
 
 function addTask(){
-    taskID=$(date +%Y-%m-%d🔹%H:%M:%S)
+    taskID=$(date +%Y-%m-%d🔸%H:%M:%S)
     task="$1"
     echo "${task}|${taskID}" >> $file
 }
 
 function splitTask(){
-    param=$(echo "$1" | sed "s/,/ /g")
+    param=$(echo "$1" | sed "s/[, ]/ /g")
     echo "$param"
 }
 
 function doneTask(){
     rows=$(splitTask "$1")
-    for row in "$rows"
+    for row in $(echo "$rows")
     do
         doneID=$(date +%Y-%m-%d🔹%H:%M:%S)
-        dTask=$(sed -n "${row}p" $file | cut -d'|' -f1)
-        echo "${dTask}|${doneID}" >> $completed
-        sed -i "${row}d" $file
+        dTask=$(sed -n "${row}p" $file)
+        echo "$(echo "$dTask" | cut -d"|" -f1)|${doneID}" >> $completed
+        sed -i "s/${dTask}//" $file
     done
+    sed -i "/^$/d" $file
 }
 
 function removeTask(){
     rows=$(splitTask "$1")
-    for row in $(echo $rows)
+    for row in $(echo "$rows")
     do
-        sed -i "${row}d" $file
+        dTask=$(sed -n "${row}p" $file)
+        sed -i "s/${dTask}//" $file
     done
+    sed -i "/^$/d" $file
 }
 
 function multipleTaskAdd(){
@@ -76,23 +87,24 @@ function multipleTaskAdd(){
     while read task; do
         [ -z "$task" ] && break
         counter+=1
-        taskID=$(date +%Y-%m-%d🔹%H:%M:%S)
+        taskID=$(date +%Y-%m-%d🔸%H:%M:%S)
         echo "${task}|${taskID}" >> $file
     done
-    printf "➕${counter} task(s) added..."
+    printf "➕${counter} task(s) added...\n"
 }
 
 function multipleTaskUpdate(){
     read -p "Enter task(s) to be updated: " rows
     ids=$(splitTask "$rows")
     counter=0
-    for id in $(echo $ids)
+    failed=0
+    for id in $(echo "$ids")
     do
         line=$(sed -n "${id}p" $file)
-        uTask=$("$line" | cut -d'|' -f1)
-        printf "${uTask}-\n"
+        uTask=$(echo "$line" | cut -d'|' -f1)
+        printf "${uTask} ➜ \n"
         read -r rTask
-        sed "s/${uTask}/${rTask}/" $file
+        sed -i "s/${uTask}/${rTask}/" $file
         if [[ "$?" -ne 0 ]]; then
             printf "❗ Not updated Properly...\n"
             failed+=1
@@ -100,18 +112,37 @@ function multipleTaskUpdate(){
             counter+=1
         fi
     done
-    printf "✔${counter} task(s) updated, ❌${failed} task(s) failed to be updated..."
+    printf "✔${counter} task(s) updated, ❌${failed} task(s) failed to be updated...\n"
+}
+
+function multipleTaskRedo(){
+    read -p "Enter task(s) to be re-done: " rows
+    ids=$(splitTask "$rows")
+    counter=0
+    for id in $(echo "$ids")
+    do
+        line=$(sed -n "${id}p" $completed)
+        rTask=$(echo "$line" | cut -d"|" -f1)
+        redoID=$(date +%Y-%m-%d🔸%H:%M:%S)
+        echo "${rTask}|${redoID}" >> $file
+        sed -i "s/${line}//" $completed
+        counter+=1
+    done
+    sed -i "/^$/d" $file
+    printf "🔁${counter} task(s) need to be done again...\n"
 }
 
 function multipleTask(){
-    printf "Mutiple task(s) to add/update:\n"
-    read -p "a/u》" operation
+    printf "Mutiple task(s) to add(a)/update(u)/redo(r):\n"
+    read -p "a/u/r》" operation
     if [[ $operation == 'a' || $operation == 'A' ]]; then
         multipleTaskAdd
     elif [[ $operation == 'u' || $operation == 'U' ]]; then
         multipleTaskUpdate
+    elif [[ $operation == 'r' || $operation == 'R' ]]; then
+        multipleTaskRedo
     else
-        printf "Enter either 'a/A' or 'u/U' for the operation..."
+        printf "Enter either 'a/A' or 'u/U' or r/R for the operation...\n"
     fi
 }
 
